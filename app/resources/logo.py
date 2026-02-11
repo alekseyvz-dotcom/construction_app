436a5cb0ee3fba6b2646ec5bdac92db734e94120
alekseyvz-dotcom <aleksey_vz@mail.ru>
2026-02-11 17:25:16 +0300

    Create logo.py

---
+++ app/resources/logo.py
@@ -0,0 +1,11 @@
+"""
+Встроенный логотип в формате Base64.
+Замените содержимое LOGO_BASE64 на свой логотип.
+"""
+
+# Минимальная PNG-заглушка 1x1 пиксель (прозрачный)
+# Замените на реальный логотип из вашего assets_logo.py
+LOGO_BASE64 = (
+    "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4"
+    "nGNgYPgPAAEDAQAIicLsAAAABJRU5ErkJggg=="
+)
